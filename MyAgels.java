6d7639417a22191d189ca36063e08f337e05581a
ELKASWANI <mohammad-kiswani@outlook.com>
2020-02-03 10:22:52 -0500

    My Age

---
+++ MyAgels.java
@@ -0,0 +1,18 @@
+public class MyAgels {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("\nHello, World , Mohammad");
+
+         int yearOfBirth;
+         int currentYear;
+         int year;
+         
+         yearOfBirth = 2001;
+         currentYear = 2020;
+         year = (currentYear - yearOfBirth);
+
+        System.out.println("\nMy Age Is " + year +".");
+}
+
+}
